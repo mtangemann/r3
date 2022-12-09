bef6d24a1078667b1667bae626d96585519fd080
Matthias Tangemann <hello@mtangemann.de>
2022-12-09 09:11:52 +0100

    test: add unit tests for `Repository.init`

--- Makefile
+++ Makefile
@@ -1,21 +1,24 @@
-.PHONY: black isort lint lint/black lint/flake8 lint/isort lint/mypy
+.PHONY: black isort lint lint/black lint/flake8 lint/isort lint/mypy test
 
 black:
-	black r3
+	black r3 test
 
 isort:
-	isort r3
+	isort r3 test
 
 lint: lint/black lint/flake8 lint/isort lint/mypy
 
 lint/black:
-	black --check r3
+	black --check r3 test
 
 lint/flake8:
-	flake8 r3
+	flake8 r3 test
 
 lint/isort:
-	isort --check r3
+	isort --check r3 test
 
 lint/mypy:
-	mypy r3
+	mypy r3 test
+
+test:
+	python -m pytest --cov=r3
---
+++ test/test_repository.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import pytest
+import yaml
+from pyfakefs.fake_filesystem import FakeFilesystem
+
+import r3
+
+
+def test_init_fails_if_path_exists(fs: FakeFilesystem) -> None:
+    path = "/path/to/repository"
+    fs.create_dir(path)
+
+    repository = r3.Repository(path)
+    with pytest.raises(FileExistsError):
+        repository.init()
+
+
+def test_init_creates_directories(fs: FakeFilesystem) -> None:
+    root = Path("/test/repository")
+    repository = r3.Repository(root)
+    repository.init()
+
+    assert root.exists()
+    assert (root / "data").exists()
+    assert (root / "jobs" / "by_hash").exists()
+
+
+def test_init_creates_config_file(fs: FakeFilesystem) -> None:
+    root = Path("/test/repository")
+    repository = r3.Repository(root)
+    repository.init()
+
+    assert (root / "r3.yaml").exists()
+
+    with open(root / "r3.yaml", "r") as config_file:
+        config = yaml.safe_load(config_file)
+
+    assert "version" in config
+    assert config["version"] == r3.__version__
