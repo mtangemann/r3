9a813172bbdacb42d31f0a20e38d3cadfe87cbcd
Matthias Tangemann <hello@mtangemann.de>
2024-03-12 11:37:09 +0100

    refactor: clean up Repository class

--- r3/repository.py
+++ r3/repository.py
@@ -1,7 +1,7 @@
-"""R3 core functionality.
+"""High-level interface to R3 repositories.
 
-This module provides the core functionality of R3. This module should not be used
-directly, but rather the public API exported by the top-level ``r3`` module.
+The `Repository` class should be imported not from this module but from the top-level
+`r3` package.
 """
 
 import os
@@ -31,8 +31,13 @@ DATE_FORMAT = r"%Y-%m-%d %H:%M:%S"
 
 
 class Repository:
+    """A repository of jobs."""
+
     def __init__(self, path: Union[str, os.PathLike]) -> None:
         """Initializes the repository instance.
+
+        Parameters:
+            path: The path to the repository.
 
         Raises:
             FileNotFoundError: If the given path does not exist.
@@ -54,7 +59,10 @@ class Repository:
 
     @staticmethod
     def init(path: Union[str, os.PathLike]) -> "Repository":
-        """Creates a repository at the given path.
+        """Creates a new repository at the given path.
+
+        Returns:
+            The newly created repository.
 
         Raises:
             FileExistsError: If the given path exists alreay.
@@ -78,8 +86,53 @@ class Repository:
         """Returns an iterator over all jobs in this repository."""
         yield from self._storage.jobs()
 
+    def __contains__(self, item: Union[Job, Dependency]) -> bool:
+        """Checks whether a job or dependency is contained in this repository.
+        
+        Parameters:
+            item: The job or dependency to check for.
+        
+        Returns:
+            Whether the given job or dependency is contained in this repository.
+        """
+        if isinstance(item, Job):
+            return item in self._storage
+
+        try:
+            resolved_item = self.resolve(item)
+        except ValueError:
+            return False
+
+        if isinstance(resolved_item, list):
+            return all(dependency in self for dependency in resolved_item)
+
+        if isinstance(resolved_item, JobDependency):
+            target = self.path / "jobs" / resolved_item.job / resolved_item.source
+            return target.exists()
+
+        if isinstance(resolved_item, GitDependency):
+            return r3.utils.git_path_exists(
+                self.path / resolved_item.repository_path,
+                resolved_item.commit,
+                resolved_item.source,
+            )
+
+        return False
+
     def commit(self, job: Job) -> Job:
+        """Commits a job to the repository.
+        
+        Parameters:
+            job: The job to commit.
+        
+        Returns:
+            The committed job. Compared to the original job, the returned job has an id
+            and the path is changed to the location in the repository.
+        """
         job = self.resolve(job)  # type: ignore
+
+        # REVIEW It would be nice if `resolve` would check whether the dependencies
+        #        exist in the repository.
         for dependency in job.dependencies:
             if dependency not in self:
                 raise ValueError(f"Missing dependency: {dependency}")
@@ -96,7 +149,12 @@ class Repository:
     def checkout(
         self, item: Union[Dependency, Job], path: Union[str, os.PathLike]
     ) -> None:
-        path = Path(path)
+        """Checks out a job or dependency to the given path.
+        
+        Parameters:
+            item: The job or dependency to check out.
+            path: The path to check out the job or dependency to.
+        """
         resolved_item = self.resolve(item)
 
         if isinstance(resolved_item, list):
@@ -106,6 +164,15 @@ class Repository:
             self._storage.checkout(resolved_item, path)
 
     def remove(self, job: Job) -> None:
+        """Removes a job from the repository.
+        
+        Parameters:
+            job: The job to remove.
+        
+        Raises:
+            ValueError: If the job is not contained in this repository or if other jobs
+                depend on it.
+        """
         if job not in self:
             raise ValueError("Job is not contained in this repository.")
 
@@ -120,24 +187,6 @@ class Repository:
 
         self._storage.remove(job)
         self._index.remove(job)
-
-    def __contains__(self, item: Union[Job, Dependency]) -> bool:
-        """Checks if the given item is contained in this repository."""
-        if isinstance(item, Job):
-            return item in self._storage
-
-        if isinstance(item, QueryDependency):
-            item = self.resolve(item)  # type: ignore
-
-        if isinstance(item, JobDependency):
-            return (self.path / "jobs" / item.job / item.source).exists()
-
-        if isinstance(item, GitDependency):
-            return r3.utils.git_path_exists(
-                self.path / item.repository_path, item.commit, item.source
-            )
-
-        return False
 
     def find(self, tags: Iterable[str], latest: bool = False) -> List[Job]:
         """Finds jobs by tags.
@@ -157,7 +206,7 @@ class Repository:
 
         The job index is used to efficiently query for jobs. The index is automatically
         updated when jobs are added or removed. This method has to be called manually
-        if the metadata of a job is changed.
+        if the metadata file of a job has been updated manually.
         """
         self._index.rebuild()
 
@@ -165,6 +214,18 @@ class Repository:
         self,
         item: Union[Job, Dependency],
     ) -> Union[Job, Dependency, List[JobDependency]]:
+        """Resolves a job or dependency.
+        
+        A job or dependency is resolved by replacing query dependencies with concrete
+        dependencies.
+
+        Parameters:
+            item: The job or dependency to resolve.
+        
+        Returns:
+            The resolved job or dependency. A query dependency might resolve to multiple
+            concrete dependencies, in which case a list of dependencies is returned.
+        """
         if item.is_resolved():
             return item
 
@@ -181,21 +242,15 @@ class Repository:
         if not isinstance(job.dependencies, list):
             raise ValueError("Dependencies are not writeable.")
 
-        resolved_dependencies = []
+        resolved_dependencies: List[Dependency] = []
 
-        for index in range(len(job.dependencies)):
-            if isinstance(job.dependencies[index], QueryDependency):
-                dependency = self._resolve_query_dependency(job.dependencies[index])
-                resolved_dependencies.append(dependency)
-            
-            elif isinstance(job.dependencies[index], QueryAllDependency):
-                dependencies = self._resolve_query_all_dependency(
-                    job.dependencies[index]
-                )
-                resolved_dependencies.extend(dependencies)
-
+        for dependency in job.dependencies:
+            resolved_dependency = self.resolve(dependency)
+            if isinstance(resolved_dependency, list):
+                resolved_dependencies.extend(resolved_dependency)
             else:
-                resolved_dependencies.append(job.dependencies[index])
+                assert isinstance(resolved_dependency, Dependency)
+                resolved_dependencies.append(resolved_dependency)
 
         job._dependencies = resolved_dependencies
         job._config["dependencies"] = [  # type: ignore
--- test/test_repository.py
+++ test/test_repository.py
@@ -3,48 +3,59 @@
 import filecmp
 import os
 import stat
+import tempfile
 from pathlib import Path
 
 import pytest
 import yaml
+from executor import execute
 from pyfakefs.fake_filesystem import FakeFilesystem
+from pytest_mock.plugin import MockerFixture
 
-import r3
+from r3.job import (
+    GitDependency,
+    Job,
+    JobDependency,
+    QueryAllDependency,
+    QueryDependency,
+)
+from r3.repository import Repository
 
 DATA_PATH = Path(__file__).parent / "data"
 
 
 @pytest.fixture
-def repository(fs: FakeFilesystem) -> r3.Repository:
-    return r3.Repository.init("/test/repository")
+def repository(fs: FakeFilesystem) -> Repository:
+    return Repository.init("/repository")
 
 
-def get_dummy_job(fs: FakeFilesystem, name: str) -> r3.Job:
+def get_dummy_job(fs: FakeFilesystem, name: str) -> Job:
     path = DATA_PATH / "jobs" / name
-    fs.add_real_directory(path, read_only=True)
-    return r3.Job(path)
+    if not fs.exists(path):
+        fs.add_real_directory(path, read_only=True)
+    return Job(path)
 
 
 def test_init_fails_if_path_exists(fs: FakeFilesystem) -> None:
-    path = "/rest/repository"
+    path = "/repository"
     fs.create_dir(path)
 
     with pytest.raises(FileExistsError):
-        r3.Repository.init(path)
+        Repository.init(path)
 
 
-def test_init_creates_directories(fs: FakeFilesystem) -> None:
-    path = Path("/test/repository")
-    r3.Repository.init(path)
+def test_init_calls_storage_init(fs: FakeFilesystem, mocker: MockerFixture) -> None:
+    storage_init = mocker.patch("r3.storage.Storage.init")
 
-    assert path.exists()
-    assert (path / "git").exists()
-    assert (path / "jobs").exists()
+    path = "/test/repository"
+    Repository.init(path)
+
+    storage_init.assert_called_once_with(Path(path))
 
 
 def test_init_creates_config_file_with_version(fs: FakeFilesystem) -> None:
     path = Path("/test/repository")
-    r3.Repository.init(path)
+    Repository.init(path)
 
     assert (path / "r3.yaml").exists()
 
@@ -54,13 +65,122 @@ def test_init_creates_config_file_with_version(fs: FakeFilesystem) -> None:
     assert "version" in config
 
 
-def test_commit_creates_job_folder(
-    fs: FakeFilesystem, repository: r3.Repository
+def test_repository_jobs_calls_storage_jobs(
+    fs: FakeFilesystem, mocker: MockerFixture
 ) -> None:
-    """Unit test for ``r3.Repository.commit``.
+    storage_jobs = mocker.patch("r3.storage.Storage.jobs")
 
-    When adding a job, a directory should be created in ``$REPOSITORY_ROOT/jobs``.
-    """
+    path = "/repository"
+    repository = Repository.init(path)
+    list(repository.jobs())
+
+    storage_jobs.assert_called_once()
+
+
+def test_repository_contains_job_calls_storage_contains(
+    fs: FakeFilesystem, mocker: MockerFixture
+) -> None:
+    storage_contains = mocker.patch("r3.storage.Storage.__contains__")
+
+    path = "/repository"
+    repository = Repository.init(path)
+    job = get_dummy_job(fs, "base")
+    job in repository  # noqa: B015
+
+    storage_contains.assert_called_once_with(job)
+
+
+def test_repository_contains_job_dependency(fs: FakeFilesystem) -> None:
+    repository = Repository.init("/repository")
+
+    dependency = JobDependency("123abc", "destination")
+    assert dependency not in repository
+
+    job = get_dummy_job(fs, "base")
+    job = repository.commit(job)
+    assert job.id is not None
+
+    dependency = JobDependency(job.id, "destination")
+    assert dependency in repository
+
+    dependency = JobDependency(job.id, "destination.py", "run.py")
+    assert dependency in repository
+
+    dependency = JobDependency(job.id, "destination.py", "does_not_exist.py")
+    assert dependency not in repository
+
+
+def test_repository_contains_git_dependency() -> None:
+    with tempfile.TemporaryDirectory() as tempdir:
+        repository = Repository.init(f"{tempdir}/repository")
+
+        dependency = GitDependency(
+            repository="https://github.com/mtangemann/r3.git",
+            commit="c2397aac3fbdca682150faf721098b6f5a47806b",
+            destination="destination",
+        )
+        assert dependency not in repository
+
+        r3_path = Path(__file__).parent.parent
+        assert (r3_path / ".git").is_dir()
+        repository_path = Path(f"{tempdir}/repository/git/github.com/mtangemann/r3")
+        execute(f"git clone {r3_path} {repository_path}")
+
+        assert dependency in repository
+
+        dependency = GitDependency(
+            repository="https://github.com/mtangemann/r3.git",
+            commit="c2397aac3fbdca682150faf721098b6f5a47806b",
+            destination="destination.py",
+            source="test/test_repository.py",
+        )
+        assert dependency in repository
+
+        dependency = GitDependency(
+            repository="https://github.com/mtangemann/r3.git",
+            commit="c2397aac3fbdca682150faf721098b6f5a47806b",
+            destination="destination.py",
+            source="does_not_exist.py",
+        )
+        assert dependency not in repository
+
+
+def test_repository_contains_query_dependency(fs: FakeFilesystem) -> None:
+    repository = Repository.init("/repository")
+
+    dependency = QueryDependency("#test", "destination")
+    assert dependency not in repository
+
+    job = get_dummy_job(fs, "base")
+    job.metadata["tags"] = ["test"]
+    job = repository.commit(job)
+
+    assert dependency in repository
+
+    dependency = QueryDependency("#test #does-not-exist", "destination")
+    assert dependency not in repository
+
+    dependency = QueryDependency("#test", "destination.py", "run.py")
+    assert dependency in repository
+
+    dependency = QueryDependency("#test", "destination.py", "does_not_exist.py")
+    assert dependency not in repository
+
+
+def test_repository_contains_query_all_dependency(fs: FakeFilesystem) -> None:
+    repository = Repository.init("/repository")
+
+    dependency = QueryAllDependency("#test", "destination")
+    assert dependency not in repository
+
+    job = get_dummy_job(fs, "base")
+    job.metadata["tags"] = ["test"]
+    job = repository.commit(job)
+
+    assert dependency in repository
+
+
+def test_commit_creates_job_folder(fs: FakeFilesystem, repository: Repository) -> None:
     job_paths = list((repository.path / "jobs").iterdir())
     assert len(job_paths) == 0
 
@@ -73,7 +193,7 @@ def test_commit_creates_job_folder(
 
 
 def test_commit_returns_the_updated_job(
-    fs: FakeFilesystem, repository: r3.Repository
+    fs: FakeFilesystem, repository: Repository
 ) -> None:
     """Unit test for ``r3.Repository.commit``.
 
@@ -90,7 +210,7 @@ def test_commit_returns_the_updated_job(
 
 
 def test_commit_copies_files_write_protected(
-    fs: FakeFilesystem, repository: r3.Repository
+    fs: FakeFilesystem, repository: Repository
 ) -> None:
     """Unit test for ``r3.Repository.commit``.
 
@@ -115,7 +235,7 @@ def test_commit_copies_files_write_protected(
 
 
 def test_commit_copies_nested_files(
-    fs: FakeFilesystem, repository: r3.Repository
+    fs: FakeFilesystem, repository: Repository
 ) -> None:
     """Unit test for ``r3.Repository.add``."""
     original_job = get_dummy_job(fs, "nested")
@@ -130,3 +250,79 @@ def test_commit_copies_nested_files(
         original_job.path / "code" / "run.py",
         shallow=False,
     )
+
+
+def test_repository_remove_fails_if_other_jobs_depend_on_job(
+    fs: FakeFilesystem, repository: Repository
+) -> None:
+    base_job = get_dummy_job(fs, "base")
+
+    job = repository.commit(base_job)
+    assert job.id is not None
+
+    dependency = JobDependency(job.id, "destination")
+    base_job._dependencies = [dependency]
+    base_job._config["dependencies"] = [dependency.to_config()]
+    dependent_job = repository.commit(base_job)
+
+    with pytest.raises(ValueError):
+        repository.remove(job)
+
+    repository.remove(dependent_job)
+    repository.remove(job)
+
+
+def test_resolve_query_dependency(fs: FakeFilesystem, repository: Repository) -> None:
+    job = get_dummy_job(fs, "base")
+    job.metadata["tags"] = ["test"]
+    job = repository.commit(job)
+
+    dependency = QueryDependency("#test", "destination")
+    resolved_dependency = repository.resolve(dependency)
+    assert isinstance(resolved_dependency, JobDependency)
+    assert resolved_dependency.job == job.id
+
+    with pytest.raises(ValueError):
+        repository.resolve(QueryDependency("#does-not-exist", "destination"))
+
+
+def test_resolve_query_all_dependency(
+    fs: FakeFilesystem,
+    repository: Repository,
+) -> None:
+    job = get_dummy_job(fs, "base")
+    job.metadata["tags"] = ["test"]
+    commited_job_1 = repository.commit(job)
+
+    dependency = QueryAllDependency("#test", "destination")
+    resolved_dependency = repository.resolve(dependency)
+    assert isinstance(resolved_dependency, list)
+    assert len(resolved_dependency) == 1
+    assert resolved_dependency[0].job == commited_job_1.id
+
+    job.metadata["tags"] = ["test", "another"]
+    committed_job_2 = repository.commit(job)
+
+    resolved_dependency = repository.resolve(dependency)
+    assert isinstance(resolved_dependency, list)
+    assert len(resolved_dependency) == 2
+    assert set([dependency.job for dependency in resolved_dependency]) == {
+        commited_job_1.id,
+        committed_job_2.id,
+    }
+
+
+def test_resolve_job(fs: FakeFilesystem, repository: Repository) -> None:
+    job = get_dummy_job(fs, "base")
+    job.metadata["tags"] = ["test"]
+    committed_job = repository.commit(job)
+
+    dependency = QueryDependency("#test", "destination")
+    job._dependencies = [dependency]
+    job._config["dependencies"] = [dependency.to_config()]
+
+    resolved_job = repository.resolve(job)
+    assert isinstance(resolved_job, Job)
+    assert all(dependency.is_resolved() for dependency in resolved_job.dependencies)
+    assert isinstance(resolved_job.dependencies[0], JobDependency)
+    assert resolved_job.dependencies[0].job == committed_job.id
